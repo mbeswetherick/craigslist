19be60302e2e0e784526a01a98c1cdae25f08e8c
Michael Beswetherick <mbeswetherick@gmail.com>
2012-04-22 16:05:27 -0700

    fixed parameter issue on update

--- data/views.py
+++ data/views.py
@@ -2,25 +2,24 @@ from data.models import *
 from django.http import HttpResponseRedirect, HttpResponse
 from django.utils import simplejson
 
+
 def create(request):
 	p = request.POST
 	post = Craiglistpost(price=p['price'], title=p['title'], url=p['url'], date=p['date'], query=p['query'], location_wide=p['location_wide'],location_region=p['location_region'],location_specific=p['location_specific'])
 	post.save()
 	return HttpResponse(simplejson.dumps(post.id))
 	
-def update(request):
+def update(request, listing_id):
 	if request.method == 'GET':
 		g = request.GET
-		listing_id = g['listing_id']
 		#url = g['url']
 		listing = Craiglistpost.object.get(id=listing_id)
-		return HttpResponse(simplejson.dumps(listing.price, listing.title, listing.url, listing.date, listing.category, listing.query, listing.location_wide, listing.location_region, listing.location_specific)
+		return HttpResponse(simplejson.dumps(listing.price, listing.title, listing.url, listing.date, listing.category, listing.query, listing.location_wide, listing.location_region, listing.location_specific))
 	else:
 		d = request.DELETE
-		listing_id = d['listing_id']
 		Craiglistpost.object.get(id=listing_id).delete()
 		return HttpResponse(simplejson.dumps(listing_id))
-
+'''
 def get(request):
 	g = request.GET
 	mean_min = g['mean_min']
@@ -32,14 +31,14 @@ def get(request):
 	range_min = g['range_min']
 	range_max = g['range_max']
 	search_for = g['search_for']
-	allPosts = Craiglistpost.objects.get()
+	allPosts = Craiglistpost.objects.filter(category=search_for)
 	category = search_for
 	if mean_exact != None:
 		mean_exact_counter = 0
 		for post in allPosts:
 			if
 
-
+'''
 
 
 
--- settings.py
+++ settings.py
@@ -56,11 +56,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -106,6 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	'/home/craigslist/data/templates'
 )
 
 INSTALLED_APPS = (
